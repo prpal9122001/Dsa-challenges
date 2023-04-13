ca1e23243718b354db3d861aa08f155d9b708b7b
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-04-13 23:45:41 +0530

    Create (dc)946. Validate Stack Sequences.cpp

---
+++ (dc)946. Validate Stack Sequences.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/validate-stack-sequences/
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int l = 0;
+        for (int i : pushed) {
+            st.push(i);
+            while (!st.empty() && st.top() == popped[l]) {
+                st.pop();
+                l++;
+            }
+        }
+        return st.empty();
+    }
+};
