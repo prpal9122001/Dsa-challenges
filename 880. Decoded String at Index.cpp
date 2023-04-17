fbf7dc02f5a618e2631deb344fe70834d5552f73
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-04-18 02:28:53 +0530

    Create 880. Decoded String at Index.cpp

---
+++ 880. Decoded String at Index.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/decoded-string-at-index/description/
+
+class Solution {
+public:
+    string decodeAtIndex(string S, int K) {
+        long N = 0, i;
+        for (i = 0; N < K; ++i)
+            N = isdigit(S[i]) ? N * (S[i] - '0') : N + 1;
+        while (i--)
+            if (isdigit(S[i]))
+                N /= S[i] - '0', K %= N;
+            else if (K % N-- == 0)
+                return string(1, S[i]);
+        return "lee215";
+    }
+};
+
