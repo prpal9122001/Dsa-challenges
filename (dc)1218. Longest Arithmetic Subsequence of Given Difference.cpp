3135d67ac1ad1effee0759684401070ac704daff
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-07-14 22:38:06 +0530

    Create (dc)1218. Longest Arithmetic Subsequence of Given Difference.cpp

---
+++ (dc)1218. Longest Arithmetic Subsequence of Given Difference.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/description/
+class Solution {
+public:
+    int longestSubsequence(vector<int>&arr, int difference) {
+
+      int ans = 1;
+      unordered_map<int,int>dp;
+      for(auto &val : arr)
+      {
+        if(!dp.count(val - difference)) dp[val] = 1;
+        else dp[val] = dp[val - difference] + 1;
+        ans = max(ans, dp[val]);
+      }
+      return ans;
+    }
+};
