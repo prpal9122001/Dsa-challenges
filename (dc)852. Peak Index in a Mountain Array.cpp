9dd3a0c48fe2cd3b0732aac2be4a6e14c8b704f3
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-07-25 22:14:33 +0530

    Create (dc)852. Peak Index in a Mountain Array.cpp

---
+++ (dc)852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0;
+        int right = arr.size() - 1;
+        int mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
