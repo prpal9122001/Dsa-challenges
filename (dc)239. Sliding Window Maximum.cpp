2f88e7839a5bacb759d81b2955c11027c697e644
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-08-16 23:58:48 +0530

    Update (dc)239. Sliding Window Maximum.cpp

--- (dc)239. Sliding Window Maximum.cpp
+++ (dc)239. Sliding Window Maximum.cpp
@@ -1,22 +1,23 @@
-//https://leetcode.com/problems/sliding-window-maximum/description/
+// Link: https://leetcode.com/problems/sliding-window-maximum/description/
 class Solution {
-    using int2=pair<int, int>; //(nums[i], i)
+    using int2 = pair<int, int>; // (nums[i], i)
 public:
-
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
-        priority_queue<int2> pq;
+        int n = nums.size();
+        priority_queue<int2> pq; // Priority queue to maintain the maximum element and its index in the window
 
-        vector<int> ans(n-k+1);
-        for (int i=0; i<k; i++)
-            pq.push({nums[i], i});
-        
-        ans[0]=pq.top().first;
-        for(int i=k; i<n; i++){
-            while(!pq.empty() && pq.top().second<=(i-k))
-                pq.pop(); //Pop up element not in the window
-            pq.push({nums[i], i});
-            ans[i-k+1]=pq.top().first;//Max element for this window
+        vector<int> ans(n - k + 1); // Vector to store the maximum elements for each window
+        for (int i = 0; i < k; i++)
+            pq.push({nums[i], i}); // Push the first k elements with their indices into the priority queue
+
+        ans[0] = pq.top().first; // Store the maximum element of the first window
+
+        for (int i = k; i < n; i++) {
+            while (!pq.empty() && pq.top().second <= (i - k))
+                pq.pop(); // Pop up element not in the current window
+
+            pq.push({nums[i], i}); // Push the current element into the priority queue
+            ans[i - k + 1] = pq.top().first; // Store the maximum element for the current window
         }
         return ans;
     }
