353b6841d29daa897ba018d198f3bac8154f19e3
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-08-26 22:44:48 +0530

    Create (dc)646. Maximum Length of Pair Chain.cpp

---
+++ (dc)646. Maximum Length of Pair Chain.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/maximum-length-of-pair-chain/description/
+class Solution {
+public:
+    int findLongestChain(std::vector<std::vector<int>>& pairs) {
+        std::sort(pairs.begin(), pairs.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
+            return a[1] < b[1];
+        });
+        
+        int cur = INT_MIN, ans = 0;
+        
+        for (const auto& pair : pairs) {
+            if (cur < pair[0]) {
+                cur = pair[1];
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
